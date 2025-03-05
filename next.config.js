593d68a7bab7c476ae0069b290e5e1d61155f22f
Glumling <rayyaan.haamid@gmail.com>
2025-03-05 01:15:13 -0600

    Ready for deployment

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/projectcleanmotion' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/projectcleanmotion/' : '',
   trailingSlash: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
